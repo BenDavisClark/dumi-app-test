7aef06498d36de73b24b34c5e2fe4933e20e3986
huyan <!?Hu654321>
2023-08-02 17:10:41 +0800

    增加配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'dumi';
 const repo = 'dumi-app-test'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/dist/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/dist/` : '/',
   themeConfig: {
     name: 'dumi-app-test',
   },
